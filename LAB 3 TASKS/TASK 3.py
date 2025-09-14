8eca53e8710a1408e0ef1283d97d7fec8b21c4df
Amnaa-Zahidd-30 <azmyno.757@gmail.com>
2025-09-14 05:31:36 -0700

    Update TASK 3.py

--- LAB 3 TASKS/TASK 3.py
+++ LAB 3 TASKS/TASK 3.py
@@ -1,1 +1,15 @@
+def employee():
+ name=input("Enter your name:")
+ salary=input("Enter your salary:")
+ if salary=="":
+    salary=10000
+ else:
+    salary=float(salary)
+
+ netsalary=salary-(salary*0.02)
+ print("Employee name: ",name)
+ print("Salary after tax: ",netsalary)
+
+employee()
+
 
