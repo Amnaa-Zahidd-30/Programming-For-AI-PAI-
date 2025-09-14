774be86d165e98a98b4545c03a6f7c3df9dc5b7e
Amnaa-Zahidd-30 <azmyno.757@gmail.com>
2025-09-14 05:35:10 -0700

    Update TASK 5.py

--- LAB 3 TASKS/TASK 5.py
+++ LAB 3 TASKS/TASK 5.py
@@ -1,1 +1,17 @@
+def check():
+    print("Enter a word to see if its palindrome or not: ")
+    s = input()
+    first = 0
+    last = len(s) - 1
+    mid = len(s) // 2
 
+    while first < mid:
+        if s[first] != s[last]:
+            print("not palindrome.")
+            return  
+        first += 1
+        last -= 1
+   
+    print("palindrome.")
+  
+check()
