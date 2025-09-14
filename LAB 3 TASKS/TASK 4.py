93bc665855bd1a560a2657743a79751e61769fe1
Amnaa-Zahidd-30 <azmyno.757@gmail.com>
2025-09-14 05:33:27 -0700

    Update TASK 4.py

--- LAB 3 TASKS/TASK 4.py
+++ LAB 3 TASKS/TASK 4.py
@@ -1,1 +1,20 @@
+list1=[]
+list2=[]
+for x in range(3):
+   print("Enter the name of keys in your list 1:")
+   keys=input()
+   list1.append(keys)
+for x in range(3):
+   print("Enter the name of values in your list 2:")
+   values=input()
+   list2.append(values)
+thisdict = {}
+for i in range(len(list1)):
+    thisdict[list1[i]] = list2[i]
+
+print("keys with their respective values are: ", thisdict)
+
+   
+
+
 
