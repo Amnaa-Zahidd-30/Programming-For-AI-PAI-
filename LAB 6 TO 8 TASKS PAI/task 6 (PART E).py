1e9cd0d1078f09546c20b7def5e716d396856232
Amnaa-Zahidd-30 <azmyno.757@gmail.com>
2025-12-02 07:35:08 -0800

    Update task 6 (PART E).py

--- LAB 6 TO 8 TASKS PAI/task 6 (PART E).py
+++ LAB 6 TO 8 TASKS PAI/task 6 (PART E).py
@@ -8,6 +8,6 @@ print (data)
 #PART E
 print("EXTRACTION OF DETAILS FROM THE DATA\n")
 print("1. Major software houses are concentrated in big cities(Islamabad, Karachi, Lahore).")
-print("2. Most companies work in software development, web development, and mobile apps.")
+print("2. Most companies work in software development, web development, and graphic designing.")
 print("3. Technologies mentioned: React, Node, PHP, Flutter, some AI/ML.")
 print("4. No revenue data available; growth trend cannot be computed.")
